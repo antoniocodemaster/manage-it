ab19c06e949b3967ddce79905e91ab851a7878d6
antonio flores <wanllyf@gmail.com>
2022-03-14 09:00:38 -0600

    Improving Read.me file and adding tasks functionalities.

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,7 +1,29 @@
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const Tasks = () => {
+  // state
+  const [tasks, changeTasks] = useState(["Bar", "Foo"]);
+
+  // functions
+  const addNewTask = () => {
+    const newTask = document.getElementById("new-task");
+    if (newTask.value != "") {
+      changeTasks([...tasks, newTask.value]);
+      newTask.value = "";
+    } else {
+      alert(`New task can't be empty`);
+    }
+  };
+
+  const removeTask = (index) => {
+    const newTasks = [...tasks];
+    console.log(`removing task`, index);
+    newTasks.splice(index, 1);
+    changeTasks(newTasks);
+  };
+
   return (
     <div className="admin-container">
       <div className="left-side-nav">
@@ -15,11 +37,33 @@ const Tasks = () => {
           <FontAwesomeIcon icon={faBell} />
           <FontAwesomeIcon icon={faMoon} />
         </div>
-        <div className="admin-box">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem, officiis. Numquam non quam id ullam ratione amet est
-          aliquam, odio vero accusamus dicta eligendi eum in sit. Rem, hic
-          nesciunt?
+        <div className="admin-box tasks-list">
+          <ul>
+            {tasks.map((task, index) => {
+              return (
+                <li key={index}>
+                  {task}
+                  <span
+                    index={index}
+                    onClick={() => {
+                      removeTask(index);
+                    }}
+                    className="close-btn"
+                  >
+                    x
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+
+          <div>
+            <label htmlFor="">New Task</label>
+            <input type="text" id="new-task" />
+          </div>
+          <div>
+            <button onClick={addNewTask}>Add New Task</button>
+          </div>
         </div>
         <div className="admin-box">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
