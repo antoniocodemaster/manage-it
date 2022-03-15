3de8f054c938686fd7927e3c4b3dda06922abc90
antonio flores <wanllyf@gmail.com>
2022-03-14 21:18:02 -0600

    Improving admin layout

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
+import profilePicture from "../../images/antonio-profile-image.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell, faMoon } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBell,
+  faMoon,
+  faPen,
+  faCloud,
+  faPowerOff,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Tasks = () => {
   // state
@@ -28,48 +36,63 @@ const Tasks = () => {
     <div className="admin-container">
       <div className="left-side-nav">
         <ul>
-          <li>Bar</li>
-          <li>Foo</li>
+          <li>
+            <FontAwesomeIcon icon={faPen} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCloud} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faHeart} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faPowerOff} />
+          </li>
         </ul>
       </div>
-      <div className="right-side-container">
+      <div className="right-side">
         <div className="top-bar">
           <FontAwesomeIcon icon={faBell} />
           <FontAwesomeIcon icon={faMoon} />
+          <img className="profile-picture" src={profilePicture} alt="" />
         </div>
-        <div className="admin-box tasks-list">
-          <ul>
-            {tasks.map((task, index) => {
-              return (
-                <li key={index}>
-                  {task}
-                  <span
-                    index={index}
-                    onClick={() => {
-                      removeTask(index);
-                    }}
-                    className="close-btn"
-                  >
-                    x
-                  </span>
-                </li>
-              );
-            })}
-          </ul>
+        <div className="widgets-container">
+          <div className="admin-box tasks-list">
+            <ul>
+              {tasks.map((task, index) => {
+                return (
+                  <li key={index}>
+                    {task}
+                    <span
+                      index={index}
+                      onClick={() => {
+                        removeTask(index);
+                      }}
+                      className="close-btn"
+                    >
+                      x
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
 
-          <div>
-            <label htmlFor="">New Task</label>
-            <input type="text" id="new-task" />
+            <div>
+              <label htmlFor="">New Task</label>
+              <input type="text" id="new-task" />
+            </div>
+            <div>
+              <button className="btn btn-primary" onClick={addNewTask}>
+                Add New Task
+              </button>
+            </div>
           </div>
-          <div>
-            <button className="btn btn-primary" onClick={addNewTask}>Add New Task</button>
+          <div className="admin-box">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, officiis. Numquam non quam id ullam ratione amet est
+            aliquam, odio vero accusamus dicta eligendi eum in sit. Rem, hic
+            nesciunt?
           </div>
-        </div>
-        <div className="admin-box">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem, officiis. Numquam non quam id ullam ratione amet est
-          aliquam, odio vero accusamus dicta eligendi eum in sit. Rem, hic
-          nesciunt?
         </div>
       </div>
     </div>
