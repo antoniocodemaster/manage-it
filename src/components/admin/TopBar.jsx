3efa30744b7ea66b84b4d27683e1ab949f6d539b
antonio flores <wanllyf@gmail.com>
2022-03-20 15:26:56 -0600

    Adding function to change the admin panel theme

--- src/components/admin/TopBar.jsx
+++ src/components/admin/TopBar.jsx
@@ -2,11 +2,11 @@ import profilePicture from "../../images/antonio-profile-image.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faMoon } from "@fortawesome/free-solid-svg-icons";
 
-const TopBar = () => {
+const TopBar = (props) => {
   return (
     <div className="top-bar">
       <FontAwesomeIcon icon={faBell} />
-      <FontAwesomeIcon icon={faMoon} />
+      <FontAwesomeIcon icon={faMoon} onClick={props.changeActiveTheme} />
       <img className="profile-picture" src={profilePicture} alt="" />
     </div>
   );
