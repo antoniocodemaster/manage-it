14bfc44c650f784f5db1e8e56fe33938fd1e4609
antonio flores <wanllyf@gmail.com>
2022-03-20 22:41:43 -0600

    Adding tooltips

--- src/components/admin/TopBar.jsx
+++ src/components/admin/TopBar.jsx
@@ -1,12 +1,25 @@
 import profilePicture from "../../images/antonio-profile-image.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faMoon } from "@fortawesome/free-solid-svg-icons";
+import ReactTooltip from "react-tooltip";
 
 const TopBar = (props) => {
   return (
     <div className="top-bar">
-      <FontAwesomeIcon icon={faBell} />
-      <FontAwesomeIcon icon={faMoon} onClick={props.changeActiveTheme} />
+      <FontAwesomeIcon
+        icon={faBell}
+        data-tip="Notifications"
+        data-for="notifications-tooltip"
+      />
+      <ReactTooltip id="notifications-tooltip" type="light" effect="solid" />
+
+      <FontAwesomeIcon
+        icon={faMoon}
+        onClick={props.changeActiveTheme}
+        data-tip="Change theme"
+        data-for="themes-tooltip"
+      />
+      <ReactTooltip id="themes-tooltip" type="light" effect="solid" />
       <img className="profile-picture" src={profilePicture} alt="" />
     </div>
   );
