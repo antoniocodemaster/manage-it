051893df1c28c3930c8f64e69347b860b888dbc5
antonio flores <wanllyf@gmail.com>
2022-03-28 21:46:24 -0600

    Installing axios and starting to make weather app request

--- src/components/admin/weather/WeatherInfo.jsx
+++ src/components/admin/weather/WeatherInfo.jsx
@@ -1,4 +1,31 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const WeatherInfo = () => {
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  const getWeatherInfo = () => {
+    const openWeatherApiBaseUrl =
+      "https://api.openweathermap.org/data/2.5/weather?";
+    const apiKey = "94b41ada67ac5b4e90e2b3a37d542f2b";
+    const lat = 35;
+    const long = 139;
+    const endpointUrl = `${openWeatherApiBaseUrl}lat=${lat}&long=${long}&appid=${apiKey}`;
+    axios({
+      method: "get",
+      url: endpointUrl,
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="admin-box">
       <p>
