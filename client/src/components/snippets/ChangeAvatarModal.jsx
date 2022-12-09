ffc63cebe22af569677ea304d2f4935810687c4f
antoniofloresonline <wanllyf@gmail.com>
2022-12-09 15:56:13 -0600

    fixing camera loading bug

--- client/src/components/snippets/ChangeAvatarModal.jsx
+++ client/src/components/snippets/ChangeAvatarModal.jsx
@@ -1,7 +1,7 @@
 import {
-   faFileImage,
-   faImagePortrait,
-   faCamera,
+  faFileImage,
+  faImagePortrait,
+  faCamera,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
@@ -10,208 +10,212 @@ import isValidImg from "../../utils/isValidImg";
 import srcToFile from "../../utils/srcToFile";
 import Modal from "./Modal";
 
-const ChangeAvatarModal = ({ isModalActive, setIsModalActive, onChangeAvatar }) => {
-   const [userImg, setUserImg] = useState(null);
-   const [imgSrc, setImgSrc] = useState("");
-   const [isAvatarImgLoading, setIsAvatarImgLoading] = useState(null);
-   const [isScreenshotModalActive, setIsScreenshotModalActive] = useState(false);
-   const [isWebcamPicActive, setIsWebcamPicActive] = useState(false);
+const ChangeAvatarModal = ({
+  isModalActive,
+  setIsModalActive,
+  onChangeAvatar,
+}) => {
+  const [userImg, setUserImg] = useState(null);
+  const [imgSrc, setImgSrc] = useState("");
+  const [isAvatarImgLoading, setIsAvatarImgLoading] = useState(null);
+  const [isScreenshotModalActive, setIsScreenshotModalActive] = useState(false);
+  const [isWebcamPicActive, setIsWebcamPicActive] = useState(false);
 
-   const userVideoRef = useRef();
+  const userVideoRef = useRef();
 
-   const handleGetImg = () => {
-      document.getElementById("upload-img").click();
-   };
+  const handleGetImg = () => {
+    document.getElementById("upload-img").click();
+  };
 
-   const handleChangeImg = ({ target }) => {
-      setIsWebcamPicActive(false);
+  const handleChangeImg = ({ target }) => {
+    setIsWebcamPicActive(false);
 
-      const [image] = target.files;
+    const [image] = target.files;
 
-      if (!isValidImg(image)) {
-         target.value = "";
+    if (!isValidImg(image)) {
+      target.value = "";
 
-         return swal({
-            title: "Invalid image",
-            icon: "error",
-            button: "Ok",
-            timer: "50000",
-         });
-      }
-
-      const fileReader = new FileReader();
-
-      fileReader.readAsDataURL(image);
-
-      fileReader.addEventListener("loadend", ({ target }) => {
-         setImgSrc(target.result);
+      return swal({
+        title: "Invalid image",
+        icon: "error",
+        button: "Ok",
+        timer: "50000",
       });
+    }
 
-      setUserImg(image);
-   };
+    const fileReader = new FileReader();
 
-   const handleCloseModal = () => {
-      setIsModalActive(false);
-   };
+    fileReader.readAsDataURL(image);
 
-   const handleUploadImg = async () => {
-      setIsAvatarImgLoading(true);
+    fileReader.addEventListener("loadend", ({ target }) => {
+      setImgSrc(target.result);
+    });
 
-      // dispatch(setAuthUser({ ...authUser, image: imgSrc }));
-      setUserImg(null);
-      setImgSrc("");
-      setIsModalActive(false);
-      setIsAvatarImgLoading(null);
-      onChangeAvatar(userImg);
-   };
+    setUserImg(image);
+  };
 
-   const handleOpenScreenshotModal = () => {
-      if (!navigator.mediaDevices?.getUserMedia) return;
+  const handleCloseModal = () => {
+    setIsModalActive(false);
+  };
 
-      setIsModalActive(false);
-      setIsScreenshotModalActive(true);
-   };
+  const handleUploadImg = async () => {
+    setIsAvatarImgLoading(true);
 
-   const handleTakeUserPic = async () => {
-      setIsWebcamPicActive(true);
+    // dispatch(setAuthUser({ ...authUser, image: imgSrc }));
+    setUserImg(null);
+    setImgSrc("");
+    setIsModalActive(false);
+    setIsAvatarImgLoading(null);
+    onChangeAvatar(userImg);
+  };
 
-      const canvas = document.createElement("canvas");
+  const handleOpenScreenshotModal = () => {
+    if (!navigator.mediaDevices?.getUserMedia) return;
 
-      canvas.width = 1920;
-      canvas.height = 1080;
+    setIsModalActive(false);
+    setIsScreenshotModalActive(true);
+  };
 
-      const c = canvas.getContext("2d");
+  const handleTakeUserPic = async () => {
+    setIsWebcamPicActive(true);
 
-      c.drawImage(userVideoRef.current, 0, 0, canvas.width, canvas.height);
+    const canvas = document.createElement("canvas");
 
-      const imgSrc = canvas.toDataURL("image/png");
+    canvas.width = 1920;
+    canvas.height = 1080;
 
-      setImgSrc(imgSrc);
+    const c = canvas.getContext("2d");
 
-      const imgFile = await srcToFile(imgSrc);
+    c.drawImage(userVideoRef.current, 0, 0, canvas.width, canvas.height);
 
-      setUserImg(imgFile);
+    const imgSrc = canvas.toDataURL("image/png");
 
-      setIsScreenshotModalActive(false);
-      setIsModalActive(true);
-   };
+    setImgSrc(imgSrc);
 
-   useEffect(() => {
-      if (!isScreenshotModalActive) return;
+    const imgFile = await srcToFile(imgSrc);
 
-      if (!navigator.mediaDevices.getUserMedia) return;
+    setUserImg(imgFile);
 
-      navigator.mediaDevices
-         .getUserMedia({ video: true })
-         .then((stream) => {
-            const video = userVideoRef.current;
+    setIsScreenshotModalActive(false);
+    setIsModalActive(true);
+  };
 
-            video.srcObject = stream;
+  useEffect(() => {
+    if (!isScreenshotModalActive) return;
 
-            video.play();
-         })
-         .catch((error) => {
-            swal({
-               title: error.message,
-               icon: "error",
-               button: "Ok",
-               timer: "4000",
-            });
-         });
-   }, [userVideoRef, isScreenshotModalActive]);
+    if (!navigator.mediaDevices?.getUserMedia) return;
 
-   useEffect(() => {
-      if (isScreenshotModalActive) return;
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        const video = userVideoRef.current;
 
-      if (userVideoRef.current) {
-         const videoStream = userVideoRef.current.srcObject;
+        video.srcObject = stream;
 
-         if(!videoStream) return;
+        video.play();
+      })
+      .catch((error) => {
+        swal({
+          title: error.message,
+          icon: "error",
+          button: "Ok",
+          timer: "4000",
+        });
+      });
+  }, [userVideoRef, isScreenshotModalActive]);
 
-         videoStream.getTracks().forEach((track) => track.stop());
-      }
-   }, [isScreenshotModalActive, userVideoRef]);
+  useEffect(() => {
+    if (isScreenshotModalActive) return;
 
-   return (
-      <>
-         <Modal
-            onClose={() => setIsScreenshotModalActive(false)}
-            isModalActive={isScreenshotModalActive}
-         >
-            <div className="screenshot-container">
-               <video ref={userVideoRef} className="user-video"></video>
-               <button
-                  onClick={handleTakeUserPic}
-                  className="btn btn-primary take-screenshot-btn"
-               >
+    if (userVideoRef.current) {
+      const videoStream = userVideoRef.current?.srcObject;
+
+      if (!videoStream) return;
+
+      videoStream.getTracks().forEach((track) => track.stop());
+    }
+  }, [isScreenshotModalActive, userVideoRef]);
+
+  return (
+    <>
+      <Modal
+        onClose={() => setIsScreenshotModalActive(false)}
+        isModalActive={isScreenshotModalActive}
+      >
+        <div className="screenshot-container">
+          <video ref={userVideoRef} className="user-video"></video>
+          <button
+            onClick={handleTakeUserPic}
+            className="btn btn-primary take-screenshot-btn"
+          >
+            <FontAwesomeIcon
+              icon={faCamera}
+              data-tip="Change theme"
+              data-for="themes-tooltip"
+            />
+          </button>
+        </div>
+      </Modal>
+      <Modal onClose={handleCloseModal} isModalActive={isModalActive}>
+        <div className="change-img-modal-content">
+          <h2>Select image</h2>
+          <div className="avatar-img-options">
+            <div onClick={handleGetImg} className="options-item upload-img">
+              <input
+                onChange={handleChangeImg}
+                id="upload-img"
+                type="file"
+                className="d-none"
+              />
+              {imgSrc && !isWebcamPicActive && (
+                <div className="avatar-img-container">
+                  <img src={imgSrc} alt="User avatar" />
+                </div>
+              )}
+              {(!imgSrc || isWebcamPicActive) && (
+                <div className="upload-img-icon">
                   <FontAwesomeIcon
-                     icon={faCamera}
-                     data-tip="Change theme"
-                     data-for="themes-tooltip"
+                    icon={faFileImage}
+                    data-tip="Change theme"
+                    data-for="themes-tooltip"
                   />
-               </button>
+                </div>
+              )}
+              <p>Upload image</p>
             </div>
-         </Modal>
-         <Modal onClose={handleCloseModal} isModalActive={isModalActive}>
-            <div className="change-img-modal-content">
-               <h2>Select image</h2>
-               <div className="avatar-img-options">
-                  <div onClick={handleGetImg} className="options-item upload-img">
-                     <input
-                        onChange={handleChangeImg}
-                        id="upload-img"
-                        type="file"
-                        className="d-none"
-                     />
-                     {imgSrc && !isWebcamPicActive && (
-                        <div className="avatar-img-container">
-                           <img src={imgSrc} alt="User avatar" />
-                        </div>
-                     )}
-                     {(!imgSrc || isWebcamPicActive) && (
-                        <div className="upload-img-icon">
-                           <FontAwesomeIcon
-                              icon={faFileImage}
-                              data-tip="Change theme"
-                              data-for="themes-tooltip"
-                           />
-                        </div>
-                     )}
-                     <p>Upload image</p>
-                  </div>
-                  <div
-                     onClick={handleOpenScreenshotModal}
-                     className="options-item take-screen-shot"
-                  >
-                     {imgSrc && isWebcamPicActive && (
-                        <div className="avatar-img-container">
-                           <img src={imgSrc} alt="User avatar" />
-                        </div>
-                     )}
-                     {(!imgSrc || !isWebcamPicActive) && (
-                        <div className="upload-img-icon">
-                           <FontAwesomeIcon
-                              icon={faImagePortrait}
-                              data-tip="Change theme"
-                              data-for="themes-tooltip"
-                           />
-                        </div>
-                     )}
-                     <p>Take picture</p>
-                  </div>
-               </div>
-               {userImg && (
-                  <button
-                     onClick={handleUploadImg}
-                     className="btn btn-primary add-img-btn"
-                  >
-                     {isAvatarImgLoading ? "Loading..." : "Add"}
-                  </button>
-               )}
+            <div
+              onClick={handleOpenScreenshotModal}
+              className="options-item take-screen-shot"
+            >
+              {imgSrc && isWebcamPicActive && (
+                <div className="avatar-img-container">
+                  <img src={imgSrc} alt="User avatar" />
+                </div>
+              )}
+              {(!imgSrc || !isWebcamPicActive) && (
+                <div className="upload-img-icon">
+                  <FontAwesomeIcon
+                    icon={faImagePortrait}
+                    data-tip="Change theme"
+                    data-for="themes-tooltip"
+                  />
+                </div>
+              )}
+              <p>Take picture</p>
             </div>
-         </Modal>
-      </>
-   );
+          </div>
+          {userImg && (
+            <button
+              onClick={handleUploadImg}
+              className="btn btn-primary add-img-btn"
+            >
+              {isAvatarImgLoading ? "Loading..." : "Add"}
+            </button>
+          )}
+        </div>
+      </Modal>
+    </>
+  );
 };
 
 export default ChangeAvatarModal;
