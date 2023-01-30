17050c61db4768931042c6b1d7778d37bdaf0a16
Antonio Flores Online <wanllyflores@gmail.com>
2023-01-30 11:50:32 -0600

    minimal changes

--- client/src/components/routers/Dashboard.jsx
+++ client/src/components/routers/Dashboard.jsx
@@ -5,34 +5,36 @@ import { changeTheme } from "../../reducers/adminReducer";
 import LeftSideNav from "../admin/layout/LeftSideNav";
 import TopBar from "../admin/layout/TopBar";
 
+//
+
 const Dashboard = () => {
-  const dispatch = useDispatch();
+   const dispatch = useDispatch();
 
-  const { authUser } = useSelector((state) => state.auth);
+   const { authUser } = useSelector((state) => state.auth);
 
-  const { activeTheme } = useSelector((state) => state.admin);
+   const { activeTheme } = useSelector((state) => state.admin);
 
-  useEffect(() => {
-    const activeThemeLS = localStorage.getItem("activeTheme") || "light-theme";
+   useEffect(() => {
+      const activeThemeLS = localStorage.getItem("activeTheme") || "light-theme";
 
-    dispatch(changeTheme(activeThemeLS));
-  }, [dispatch]);
+      dispatch(changeTheme(activeThemeLS));
+   }, [dispatch]);
 
-  useEffect(() => {
-    if (!activeTheme) return;
+   useEffect(() => {
+      if (!activeTheme) return;
 
-    localStorage.activeTheme = activeTheme;
-  }, [activeTheme]);
+      localStorage.activeTheme = activeTheme;
+   }, [activeTheme]);
 
-  return (
-    <div className={`admin-container ${activeTheme}`}>
-      <LeftSideNav />
-      <div className="right-side">
-        <TopBar />
-        {authUser?.id ? <Outlet /> : <Navigate to="/" />}
+   return (
+      <div className={`admin-container ${activeTheme}`}>
+         <LeftSideNav />
+         <div className="right-side">
+            <TopBar />
+            {authUser?.id ? <Outlet /> : <Navigate to="/" />}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Dashboard;
